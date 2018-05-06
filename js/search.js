a5593fba0e50b04521aa98415f7cecec23f2ed73
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-05-07 00:15:34 +0800

    add google

--- js/search.js
+++ js/search.js
@@ -2,6 +2,9 @@
 write.onkeypress = function(e){
     e.stopPropagation()
     baidu.onclick = function(){
-        window.open('http://www.baidu.com/s?wd=' + write.value, '_blank')
+        window.open('//www.baidu.com/s?wd=' + write.value, '_blank')
+    }
+    google.onclick = function(){
+        window.open('//www.google.com/search?q=' + write.value, '_blank')
     }
 }
